a48a0820c02b46e1224eeebd98781bb1657a8677
Public <Public@LAPTOP-CUE54A45>
2022-09-11 13:12:06 +0530

    First Commit...

---
+++ src/gitDemo/GitTest.java
@@ -0,0 +1,17 @@
+package gitDemo;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+	
+	String Thirsty="Yes";
+	
+	
+		if(Thirsty=="Yes") {
+			System.out.println("get some water");
+		}
+		else {
+			System.out.println("Carry On");
+		}
+	}
+	}
